8f5571cc1ef60b8f75a9e4abbdd573530ea1730d
Mikko Kujala <m.kujala@live.com>
2025-07-03 21:26:18 +0300

    Remove c_to_SensorIdEnum(..)

--- src/c-include/node_c_type_conversions.h
+++ src/c-include/node_c_type_conversions.h
@@ -9,7 +9,6 @@
 // C->NAPI
 napi_value c_to_NapiSensorEvent(napi_env env, sh2_SensorEvent_t *ev);
 napi_value c_to_ProductId(napi_env env, sh2_ProductId_t *id);
-napi_value c_to_SensorIdEnum(napi_env env);
 napi_value c_to_SensorConfig(napi_env env, sh2_SensorConfig_t *cfg);
 napi_value c_to_SensorEvent(napi_env env, sh2_SensorEvent_t *ev);
 napi_value c_to_AsyncEventId(napi_env env);
