2d1fe4c41b8b515f017edd45ea06523775acd363
Mikko Kujala <m.kujala@live.com>
2025-07-04 14:24:17 +0300

    Updated error strings to match their names

--- src/c-src/error.c
+++ src/c-src/error.c
@@ -1,19 +1,18 @@
-const char* ERROR_OPENING_DEVICE = "could not open device";
-const char* ERROR_ACQUIRING_SLAVE = "could not set fd to slave addr";
-const char* ERROR_WRITING_TO_SLAVE = "could not write to slave";
-const char* ERROR_READING_FULL_BUFFER =
-    "could not read the whole designated message size";
-const char* ERROR_INTERACTING_WITH_DRIVER = "some error happened with driver";
+const char* ERROR_OPENING_DEVICE = "ERROR_OPENING_DEVICE";
+const char* ERROR_ACQUIRING_SLAVE = "ERROR_ACQUIRING_SLAVE";
+const char* ERROR_WRITING_TO_SLAVE = "ERROR_WRITING_TO_SLAVE";
+const char* ERROR_READING_FULL_BUFFER = "ERROR_READING_FULL_BUFFER";
+const char* ERROR_INTERACTING_WITH_DRIVER = "ERROR_INTERACTING_WITH_DRIVER";
 
-const char* ARGUMENT_ERROR = "argument error";
-const char* UNKNOWN_ERROR = "unknown error happened with nodejs";
+const char* ARGUMENT_ERROR = "ARGUMENT_ERROR";
+const char* UNKNOWN_ERROR = "UNKNOWN_ERROR";
 const char* ERROR_TRANSLATING_STRUCT_TO_NODE =
-    "could't translate value(s) in C struct as node values";
+    "ERROR_TRANSLATING_STRUCT_TO_NODE";
 const char* ERROR_TRANSLATING_NODE_TO_STRUCT =
-    "could't translate value(s) in node value to C struct";
-const char* ERROR_CREATING_NAPI_VALUE = "couldn't create a napi value";
-const char* REF_ERROR = "Unexpected ref count";
-const char* SENSOR_REPORT_ERROR = "error constructing report object";
+    "ERROR_TRANSLATING_NODE_TO_STRUCT";
+const char* ERROR_CREATING_NAPI_VALUE = "ERROR_CREATING_NAPI_VALUE";
+const char* REF_ERROR = "REF_ERROR";
+const char* SENSOR_REPORT_ERROR = "SENSOR_REPORT_ERROR";
 
-const char* ERROR_EXECUTING_TEST = "bno08x bad test";
-const char* ASSERT_ERROR = "bno08x test failed assertion";
+const char* ERROR_EXECUTING_TEST = "ERROR_EXECUTING_TEST";
+const char* ASSERT_ERROR = "ASSERT_ERROR";
